6d7c91146e4d4d21bc5857d468e88a377fab15db
akanksha-00 <more.akanksha2000@gmail.com>
2023-04-10 19:34:34 +0530

    modified

--- paho_client.py
+++ paho_client.py
@@ -1,23 +1,40 @@
 import time
 import paho.mqtt.client as paho
-broker = "10.50.2.49"
+
+
+broker_address = "broker_address"
+
+
 #define callback
 def on_message(client, userdata, message):
     time.sleep(1)
     print("received message =",str(message.payload.decode("utf-8")))
 
-client= paho.Client("client-001") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
-######Bind function to callback
+#create client object client1.on_publish = on_publish 
+client= paho.Client("client-001") 
+
+#Bind function to callback
 client.on_message=on_message
-#####
-print("connecting to broker ",broker)
-client.connect(broker, port=139)#connect
-client.loop_start() #start loop to process received messages
-print("subscribing ")
-client.subscribe("house/bulb1")#subscribe
+
+#connect
+print("connecting to broker ", broker_address)
+client.connect(broker_address, port=1883)
+
+#start loop to process received messages
+client.loop_start() 
+
+#subscribe
+print("subscribing to topic bulb1")
+client.subscribe("house/bulb1")
 time.sleep(2)
-print("publishing ")
-client.publish("house/bulb1","on")#publish
+
+#publish
+print("publishing to topic bulb1")
+client.publish("house/bulb1","on")
 time.sleep(4)
-client.disconnect() #disconnect
-client.loop_stop() #stop loop
+
+#disconnect
+client.disconnect() 
+
+#stop loop
+client.loop_stop() 
